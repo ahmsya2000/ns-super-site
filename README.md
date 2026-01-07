b0093513dc638f669699bec87a61e565258e6591
ahmsya2000 <ahmsya2000@gmail.com>
2026-01-07 16:33:06 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# NS Super Website
+
+Next.js multi-business website (Appareal, Studio, Aircare).
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1>NS Super Website</h1>
+      <p>Appareal • Studio • Aircare</p>
+    </main>
+  );
+}
